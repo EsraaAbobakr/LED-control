4c87f905a72861c93b2e187bb151276666eb48b6
EsraaAbobakr <esraanasser95@hotmail.com>
2022-09-20 13:53:14 +0200

    update

--- Src/MCAL/Inc/GPIO.h
+++ Src/MCAL/Inc/GPIO.h
@@ -58,7 +58,16 @@
 * \Return value:   : None
 *******************************************************************************/
 void GPIO_Init (uint8);
- 
+extern void Gpio_Init(const GpioConfiguration* const cnfg_table,
+		uint8_t cnfgTable_size);
+
+extern void Gpio_Dir_set(GpioPorts port, GpioPins pin,
+		GpioPinDirection_t direction);
+extern GpioPinDirection_t Gpio_Dir_get(GpioPorts port, GpioPins pin);
+
+extern void Gpio_PinWrite(GpioPorts port, GpioPins pin, GpioPinState_t state);
+extern void Gpio_PinToggle(GpioPorts port, GpioPins pin);
+extern int32_t Gpio_PinRead(GpioPorts port, GpioPins pin);
 #endif  /* IntCrtl_H */
 
 /**********************************************************************************************************************
