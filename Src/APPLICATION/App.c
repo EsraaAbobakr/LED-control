f35e6f163b80056ec7f248356d69ec5052247f3b
EsraaAbobakr <esraanasser95@hotmail.com>
2022-09-25 23:53:06 +0200

    update

--- Src/APPLICATION/App.c
+++ Src/APPLICATION/App.c
@@ -2,8 +2,8 @@
 
 int main(void)
 {
-	Data_types_Ticks TicksON  = (T_ON_SEC  *1000);
-	Data_types_Ticks TicksOFF = (T_OFF_SEC *1000);
+	Data_types_Ticks TicksON  = 0x03;/*(T_ON_SEC  *1000);*/
+	Data_types_Ticks TicksOFF = 0x03;/*(T_OFF_SEC *1000);*/
 	while(1)
 	{
 		Blink_Led(TicksON,TicksOFF);
